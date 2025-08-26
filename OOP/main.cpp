38ca419cd1188635bb32d04f22a9ab239d7172e9
Ulugbek <u.nishonxodjaev@newuu.uz>
2025-08-26 11:43:48 +0500

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Ulugbekjon Nishonxodjaev on 26/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
